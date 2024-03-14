2f2bc4e18d1c6a2908137cc5baf1d3569b0c028f
SumukhNitundila <sumukh14@gmail.com>
2024-03-14 14:15:18 -0500

    Made it so that codeium can give multi-line recommendations

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -31,7 +31,6 @@ require('mason-lspconfig').setup({
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load()
@@ -40,20 +39,54 @@ require('luasnip.loaders.from_vscode').lazy_load()
 local lspkind = require('lspkind')
 -- lspkind.init({})
 
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
 cmp.setup({
     sources = {
         { name = 'codeium' },
         { name = 'nvim_lsp' },
+        { name = 'luasnip', keyword_length = 5 },
         { name = 'nvim_lua' },
         { name = 'path' },
-        { name = 'luasnip', keyword_length = 2 },
         { name = 'buffer',  keyword_length = 3 },
+    },
+    experimental = {
+        ghost_text = true,
+    },
+    window = {
+        completion = {
+            border = border("WinBorder"),
+            -- winhighlight = "CursorLine:PmenuSel,Search:None",
+            winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        },
+        -- documentation = true
+        documentation = {
+            border = border("FloatBorder"),
+            winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        },
+    },
+    snippet = {
+        expand = function(args)
+            -- if not ls then return end
+            require('luasnip').lsp_expand(args.body)
+        end
     },
     formatting = {
         format = lspkind.cmp_format({
-            mode = "symbol_text", 
+            mode = "symbol_text",
             preset = "codicons",
-            maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
             ellipsis_char = '...',
             show_labelDetails = true,
             symbol_map = { Codeium = "", }
@@ -65,4 +98,6 @@ cmp.setup({
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
+    max_lines = 5
 })
+
